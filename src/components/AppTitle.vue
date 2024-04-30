56fed9dd5bd53386f52bc8f4d6629d4576d5951a
danielemantovani <daniele.mantovani124@gmail.com>
2024-04-30 18:34:30 +0200

    feat: stampa in pagina

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+
+export default {
+    data (){
+        return {
+            title: "La mia prima app con Vite",
+    }
+    }
+}
+</script>
+
+<template>
+<h1>{{ title }}</h1>
+<img src="" alt="">
+</template>
+
+<style>
+
+</style>
